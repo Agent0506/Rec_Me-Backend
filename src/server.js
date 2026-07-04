7690f2ea01dbf69e3fdd48626d458f5834c4c622
Michal Nový <mnovy222012@gmail.com>
2026-07-04 17:49:16 +0200

    Added RAM storage

--- src/server.js
+++ src/server.js
@@ -6,19 +6,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const players = [];
+const players = {};
 
 app.get('/health', (req, res) => {
   res.status(200).send("OK; Server is running");
 });
 
-app.get('/me', verifyUnityToken,  (req, res) => {
-  res.json({
-    playerId: req.playerId,
-    name: "player",
-    level: 1,
-    coins: 200
-  })
+app.get('/me', verifyUnityToken, (req, res) => {
+  const id = req.playerId;
+
+  if (!players[id]) {
+    players[id] = {
+      playerId: id,
+      name: "player_" + id.slice(0, 5),
+      level: 1,
+      coins: 200,
+      createdAt: Date.now()
+    };
+  }
+
+  res.json(players[id]);
+});
+
+app.get('/debug/players', (req, res) => {
+  res.json(players);
 });
 
 const PORT = process.env.PORT || 3000;
