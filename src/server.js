2233dc07eeaacca3be0eeb17ab9e2700a7891125
Michal Nový <mnovy222012@gmail.com>
2026-07-04 18:09:24 +0200

    Feature: added pg database

--- src/server.js
+++ src/server.js
@@ -1,35 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 import { verifyUnityToken } from "./middleware/auth.js";
+import { pool } from "./db.js"
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const players = {};
-
 app.get('/health', (req, res) => {
   res.status(200).send("OK; Server is running");
 });
 
-app.get('/me', verifyUnityToken, (req, res) => {
+app.get('/me', verifyUnityToken, async (req, res) => {
   const id = req.playerId;
 
-  if (!players[id]) {
-    players[id] = {
-      playerId: id,
-      name: "player_" + id.slice(0, 5),
-      level: 1,
-      coins: 200,
-      createdAt: Date.now()
-    };
+  const result = await pool.query(
+    "SELECT * FROM players WHERE player_id = $1",
+    [id]
+  );
+
+  if (result.rows.length === 0) {
+    const newPlayer = await pool.query(
+      "INSERT INTO players (player_id, name, level, coins, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [id, "player_" + id.slice(0, 5), 1, 0, Date.now()]
+    );
+
+    return res.json(newPlayer.rows[0]);
   }
 
-  res.json(players[id]);
-});
-
-app.get('/debug/players', (req, res) => {
-  res.json(players);
+  res.json(result.rows[0]);
 });
 
 const PORT = process.env.PORT || 3000;
