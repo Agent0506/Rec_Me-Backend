3b8c44b8c0a3d027c661fe30f60ee9a3ffcd3402
Michal Nový <mnovy222012@gmail.com>
2026-07-04 17:06:42 +0200

    Bugfix #4 added real data

--- src/server.js
+++ src/server.js
@@ -6,12 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ status: "ok", message: "Rec Me backend is running"});
+const players = [];
+
+app.get('/health', (req, res) => {
+  res.status(200).send("OK; Server is running");
+});
+
+app.get('/me', (req, res) => {
+  const id = req.query.id;
+
+  if (!players[id]) {
+    players[id] = {
+      name : "Player " + id,
+      level : 1,
+      coins: 200
+    };
+  }
+
+  res.json(players[id]);
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("App is running on" + PORT);
+  console.log("App is running on " + PORT);
 });
