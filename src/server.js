89ff540a8915541e84fc99b2d5cb8a3d35416e5f
Michal Nový <mnovy222012@gmail.com>
2026-07-04 18:47:30 +0200

    Added Rate Limit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import rateLimit from "express-rate-limit";
+
 import { verifyUnityToken } from "./middleware/auth.js";
 import { pool } from "./db.js"
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 app.get('/health', (req, res) => {
   res.status(200).send("OK; Server is running");
