f6e797a51fac6e2308388d2ca1f6a780364c94c9
Michal Nový <mnovy222012@gmail.com>
2026-07-17 22:43:34 +0200

    Added token verification

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,44 @@
-import jwt from 'jsonwebtoken';
+import { createRemoteJWKSet, jwtVerify } from "jose";
 
-export const verifyUnityToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+const JWKS = createRemoteJWKSet(
+    new URL("https://player-auth.services.api.unity.com/.well-known/jwks.json")
+);
 
-  if (!authHeader)
-    return res.status(401).json({ error: "Missing token" })
-  
-  const token = authHeader.split(" ")[1];
+export async function verifyUnityToken(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
 
-  try {
-    const decode = jwt.decode(token);
+        if (!authHeader?.startsWith("Bearer ")) {
+            return res.status(401).json({
+                error: "Missing bearer token"
+            });
+        }
 
-    req.playerId = decode?.sub;
+        const token = authHeader.substring(7);
 
-    next();
-  }
-  catch(err) {
-    return res.status(401).json({ error:"Invalid token" })
-  }
+        const { payload } = await jwtVerify(token, JWKS, {
+            issuer: "https://player-auth.services.api.unity.com",
+        });
+
+        // Dočasně vypiš payload do logů
+        console.log("Unity JWT payload:", payload);
+
+        if (!payload.sub) {
+            return res.status(401).json({
+                error: "Token does not contain 'sub' claim"
+            });
+        }
+
+        req.playerId = payload.sub;
+        req.user = payload;
+
+        next();
+
+    } catch (err) {
+        console.error("JWT verification failed:", err);
+
+        return res.status(401).json({
+            error: "Invalid or expired token"
+        });
+    }
 }
--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/me', verifyUnityToken, async (req, res) => {
+  try {
   const id = req.playerId;
 
   const result = await pool.query(
@@ -40,6 +41,13 @@ app.get('/me', verifyUnityToken, async (req, res) => {
   }
 
   res.json(result.rows[0]);
+}
+catch {
+  console.error(err);
+  res.status(500).json({
+      error: "Internal server error"
+  });
+}
 });
 
 const PORT = process.env.PORT || 3000;
